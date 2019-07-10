fb00ce24f2d2eeefb952361ac98359ffd0429eea
Tomasz <tomgal144@gmail.com>
2019-07-10 23:11:18 +0200

    Small improvements

--- src/main/java/com/galuszkat/http/Configuration.java
+++ src/main/java/com/galuszkat/http/Configuration.java
@@ -9,13 +9,17 @@ import lombok.Setter;
 @Builder
 public class Configuration {
 
+    @Builder.Default
     private String browserName = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20120427 Firefox/15.0a1";
+    @Builder.Default
     private String encoding = "UTF-8";
     // Proxy settings
+    @Builder.Default
     private boolean userProxy = false;
     private String proxyType;
     private String proxyIp;
     private int proxyPort;
     // Sleep time between each request
+    @Builder.Default
     private int sleep = 100;
 }
--- src/test/java/com/galuszkat/http/HttpClientTest.java
+++ src/test/java/com/galuszkat/http/HttpClientTest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.Test;
 
 class HttpClientTest {
 
-    private final HttpClient client = new HttpClient(Configuration.builder()
-            .encoding("UTF-8")
-            .browserName("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20120427 Firefox/15.0a1")
-            .build());
+    private final HttpClient client = new HttpClient(Configuration.builder().build());
     private static final String TEST_URL = "https://www.otodom.pl/sprzedaz/mieszkanie/krakow/?search%5Bfilter_float_price%3Afrom%5D=350000&search%5Bfilter_float_price%3Ato%5D=450000&search%5Bfilter_float_m%3Afrom%5D=25&search%5Bfilter_float_m%3Ato%5D=150&search%5Bfilter_enum_market%5D%5B0%5D=primary&search%5Bfilter_enum_market%5D%5B1%5D=secondary&search%5Bdescription%5D=1&search%5Bdistrict_id%5D=126939&search%5Bcity_id%5D=38&search%5Bsubregion_id%5D=410&search%5Bregion_id%5D=6";
 
     @Test
